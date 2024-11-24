c429753f357fbc0f4136312681dda2b73c9fdf49
Pierre Lasante <pierre.lasante@videotron.ca>
2024-11-24 10:50:43 -0500

    Terminer de designer la page Register.jsx

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React,{lazy} from 'react';
+import React, {lazy} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {BrowserRouter} from "react-router-dom";
-const App = lazy(()=> import('./App'));
+
+const App = lazy(() => import('./App'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 );
 
