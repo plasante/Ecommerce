70bba6b8b1d35b0522b8b453b0847f683b921a06
Pierre Lasante <pierre.lasante@videotron.ca>
2024-11-24 11:41:02 -0500

    form submit is working for the register page

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -4,28 +4,48 @@ import {FaGoogle} from 'react-icons/fa';
 import {FaFacebook} from 'react-icons/fa';
 
 const Register = () => {
+
+    const [state, setState] = React.useState({
+        name: '',
+        email: '',
+        password: '',
+    })
+
+    const inputHandler = (event) => {
+        setState({
+            ...state,
+            [event.target.name]: event.target.value,
+
+        })
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        console.log(state);
+    }
+
     return (
         <div className={'min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center'}>
             <div className={'w-[350px] text-[#ffffff] p-2'}>
                 <div className={'bg-[#6f68d1] p-4 rounded-md'}>
                     <h2 className={'text-xl mb-3 font-bold'}>Welcome to Ecommerce</h2>
                     <p className={'text-sm mb-3 font-medium'}>Please register your account</p>
-                    <form>
+                    <form onSubmit={submitHandler}>
                         <div className={'flex flex-col w-full gap-1 mb-3'}>
                             <label htmlFor="name">Name</label>
-                            <input
+                            <input onChange={inputHandler} value={state.name}
                                 className={'px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md '}
                                 type={'text'} name={'name'} placeholder={'Name'} id={'name'} required/>
                         </div>
                         <div className={'flex flex-col w-full gap-1 mb-3'}>
                             <label htmlFor="email">Email</label>
-                            <input
+                            <input onChange={inputHandler} value={state.email}
                                 className={'px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md '}
                                 type={'text'} name={'email'} placeholder={'Email'} id={'email'} required/>
                         </div>
                         <div className={'flex flex-col w-full gap-1 mb-3'}>
                             <label htmlFor="password">Password</label>
-                            <input
+                            <input onChange={inputHandler} value={state.password}
                                 className={'px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md '}
                                 type={'password'} name={'password'} placeholder={'Password'} id={'password'} required/>
                         </div>
