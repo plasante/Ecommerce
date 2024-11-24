f02d5265f732409de7091d04af57b130da797822
Pierre Lasante <pierre.lasante@videotron.ca>
2024-11-24 08:57:24 -0500

    Implemented routes for login and register

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React,{lazy} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {BrowserRouter} from "react-router-dom";
+const App = lazy(()=> import('./App'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
