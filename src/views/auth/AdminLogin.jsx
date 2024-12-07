d238321707c23a05560640c3990810fee2eb1c17
Pierre Lasante <pierre.lasante@videotron.ca>
2024-12-07 09:37:28 -0500

    showing invalid error messages

--- src/views/auth/AdminLogin.jsx
+++ src/views/auth/AdminLogin.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {admin_login} from "../../store/Reducers/authReducer";
+import {admin_login, messageClear} from "../../store/Reducers/authReducer";
 import { PropagateLoader } from "react-spinners";
+import toast from "react-hot-toast";
 
 
 const AdminLogin = () => {
 
     const dispatch = useDispatch();
-    const {loader} = useSelector(state => state.auth)
+    const {loader, errorMessage} = useSelector(state => state.auth)
 
     const [state, setState] = React.useState({
         email: '',
@@ -34,6 +35,13 @@ const AdminLogin = () => {
         justifyContent: 'center',
         alignItem: 'center'
     }
+
+    useEffect(() => {
+        if (errorMessage) {
+            toast.error(errorMessage);
+            dispatch(messageClear())
+        }
+    },[errorMessage,dispatch]);
 
     return (
         <div className={'min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center'}>
